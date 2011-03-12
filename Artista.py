864e818cf97b256021daf52fadfb73e71acd1ce6
Diego Rangel <diegords@gmail.com>
2011-03-12 19:43:09 -0300

    Versao oito do minimundo3

---
+++ Artista.py
@@ -0,0 +1,31 @@
+class Artista:
+    codA=None
+    nomeA=None
+    paisOrigem=None
+    dataNascimento=None
+    
+    def setCodA(self,codA):
+        self.codA=codA
+    
+    def getCodA(self):
+        return self.codA
+    
+    def setnomeA(self,nomeA):
+        self.nomeA=nomeA
+    
+    def getnomeA(self):
+        return self.nomeA
+    
+    def setpaisOrigem(self,paisOrigem):
+        self.paisOrigem=paisOrigem
+    
+    def getpaisOrigem(self):
+        return self.paisOrigem
+    
+    def setdataNascimento(self,dataNascimento):
+        self.dataNascimento=dataNascimento
+    
+    def getdataNascimento(self):
+        return self.dataNascimento
+
+
---
+++ Diretor.py
@@ -0,0 +1,29 @@
+class Diretor:
+    codD=None
+    nomeD=None
+    paisOrigem=None
+    dataNascimento=None
+    
+    def setCodD(self,codD):
+        self.codD=codD
+    
+    def getCodD(self):
+        return self.codD
+    
+    def setnomeD(self,nomeD):
+        self.nomeD=nomeD
+    
+    def getnomeD(self):
+        return self.nomeD
+    
+    def setpaisOrigem(self,paisOrigem):
+        self.paisOrigem=paisOrigem
+    
+    def getpaisOrigem(self):
+        return self.paisOrigem
+    
+    def setdataNascimento(self,dataNascimento):
+        self.dataNascimento=dataNascimento
+    
+    def getdataNascimento(self):
+        return self.dataNascimento
