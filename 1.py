2c83caa51cb0038cc6423400763756619841b91a
Diego Rangel <diegords@gmail.com>
2011-03-12 15:05:53 -0300

    Versao cinco do minimundo3

--- 1.py
+++ 1.py
@@ -1,88 +1,81 @@
 import unittest
-from Socio import Socio
-from Filme import Filme
-from Copia import Copia
+from Socio3 import Socio3
+from Filme3 import Filme3
+from Copia3 import Copia3
 
 class TestSocio(unittest.TestCase):
+    
+    def setUp(self):
+        self.socio = Socio3()
+        
     def testSocioExiste(self):
-        socio = Socio()
-        assert socio != None, "Socio nao existe"
+        assert self.socio != None, "Socio nao existe"
         
     def testNumInscricao(self):
-        socio = Socio()
-        assert socio.numIns != None,"Numero de inscricao nao existe"
+        assert self.socio.numIns != None,"Numero de inscricao nao existe"
         
     def testNome(self):
-        socio = Socio()
-        assert socio.nome != None,"Nome nao existe"
+        assert self.socio.nome != None,"Nome nao existe"
         
     def testEndereco(self):
-        socio = Socio()
-        assert socio.end != None,"Endereco nao existe"
+        assert self.socio.end != None,"Endereco nao existe"
     
     def testTelefone(self):
-        socio = Socio()
-        assert socio.tel != None,"Telefone nao existe"
+        assert self.socio.tel != None,"Telefone nao existe"
 
 
 class TestFilme(unittest.TestCase): 
+    
+    def setUp(self):
+        self.filme = Filme3()
+        
     def testFilmeExiste(self):
-        filme = Filme()
-        assert filme != None, "Filme nao existe"
+        assert self.filme != None, "Filme nao existe"
 
     def testCodigo(self):
-        filme = Filme()
-        assert filme.cod != None,"Codigo nao existe"
+        assert self.filme.cod != None,"Codigo nao existe"
         
     def testTitulo(self):
-        filme = Filme()
-        assert filme.titulo != None,"Titulo nao existe"
+        assert self.filme.titulo != None,"Titulo nao existe"
         
     def testDuracao(self):
-        filme = Filme()
-        assert filme.duracao != None,"Duracao nao existe"
+        assert self.filme.duracao != None,"Duracao nao existe"
     
     def testAno(self):
-        filme = Filme()
-        assert filme.ano != None,"Ano nao existe"
+        assert self.filme.ano != None,"Ano nao existe"
     
     def testGenero(self):
-        filme = Filme()
-        assert filme.genero != None,"Genero nao existe"
+        assert self.filme.genero != None,"Genero nao existe"
         
     def testDiretor(self):
-        filme = Filme()
-        assert filme.diretor != None,"Diretor nao existe"
+        assert self.filme.diretor != None,"Diretor nao existe"
         
     def testArtista1(self):
-        filme = Filme()
-        assert filme.artista1 != None,"Artista1 nao existe"
+        assert self.filme.artista1 != None,"Artista1 nao existe"
     
     def testArtista2(self):
-        filme = Filme()
-        assert filme.artista2 != None,"Artista2 nao existe"
+        assert self.filme.artista2 != None,"Artista2 nao existe"
     
 
-class TestCopia(unittest.TestCase):        
+class TestCopia(unittest.TestCase):  
+    
+    def setUp(self):
+        self.copia = Copia3()
+              
     def testCopiaExiste(self):
-        copia = Copia()
-        assert copia != None, "Copia nao existe"
+        assert self.copia != None, "Copia nao existe"
             
     def testCodigo(self):
-        copia = Copia()
-        assert copia.cod != None,"Codigo nao existe"
+        assert self.copia.cod != None,"Codigo nao existe"
     
     def testDataAquisicao(self):
-        copia = Copia()
-        assert copia.dataAq != None,"Data de Aquisicao nao existe"
+        assert self.copia.dataAq != None,"Data de Aquisicao nao existe"
     
     def testEstado(self):
-        copia = Copia()
-        assert copia.estado != None,"Estado nao existe"
+        assert self.copia.estado != None,"Estado nao existe"
     
     def testListaEmprestimos(self):
-        copia = Copia()
-        assert copia.listaE != None,"Lista de Emprestimos nao existe"
+        assert self.copia.listaE != None,"Lista de Emprestimos nao existe"
         
 
         
