ec843de265c5de52fc794d8036d7e891426512a3
Diego Rangel <diegords@gmail.com>
2011-03-12 10:41:27 -0300

    Versao um do minimundo3

---
+++ 1.py
@@ -0,0 +1,19 @@
+import unittest
+
+class TestSocio(unittest.TestCase):
+    def testSocioExiste(self):
+        socio = Socio()
+        assert socio != None, "Socio nao existe"
+
+class TestFilme(unittest.TestCase):  
+    def testFilmeExiste(self):
+        filme = Filme()
+        assert filme != None, "Filme nao existe"
+
+class TestCopia(unittest.TestCase):
+    def testCopiaExiste(self):
+        copia = Copia()
+        assert copia != None, "Copia nao existe"
+        
+if  __name__=="__main__":
+    unittest.main() 
---
+++ Socio.py
@@ -0,0 +1,2 @@
+class Socio:
+    pass
